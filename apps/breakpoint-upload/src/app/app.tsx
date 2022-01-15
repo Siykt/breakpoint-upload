9aedf03699154e38a67cab26cd060194c175d007
siykt <cnsiykt@163.com>
2022-01-15 14:45:28 +0800

    feat: 第一版大文件上传

--- apps/breakpoint-upload/src/app/app.tsx
+++ apps/breakpoint-upload/src/app/app.tsx
@@ -1,25 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { Message } from '@breakpoint-upload/api-interfaces';
+import React, { useCallback, useState } from 'react';
+import Menu, { MenuItem } from './components/Menu';
+import BreakpointFileUpload from './components/BreakpointFileUpload';
+import Settings from './components/Settings';
+import { Typography } from 'antd';
+
+const { Title, Paragraph, Text, Link } = Typography;
 
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
+  const [view, setView] = useState(MenuItem.main);
+  const handleToggle = (key: MenuItem) => {
+    setView(key);
+  };
 
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
+  const Container = useCallback(
+    () =>
+      view === MenuItem.main ? (
+        <>
+          <Title>Upload 简介</Title>
+          <Paragraph>
+            此 Demo 用于展示如何使用 Nodejs 实现
+            <Text strong> 大文件的切片上传、断点上传以及文件秒传 </Text>
+            技术
+          </Paragraph>
+          <Paragraph>
+            技术栈:
+            <Link className="ml10">React</Link>
+            <Link className="ml10">AntDesign</Link>
+            <Link className="ml10">Express</Link>
+          </Paragraph>
+          <Title>实例</Title>
+          <Text type="secondary">代码实现参见 BreakpointFileUpload 组件</Text>
+          <div className="mt10">
+            <BreakpointFileUpload />
+          </div>
+        </>
+      ) : (
+        <>
+          <Title>上传配置</Title>
+          <Settings />
+        </>
+      ),
+    [view]
+  );
 
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to breakpoint-upload!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-        />
+      <style jsx global>{`
+        .ml10 {
+          margin-left: 10px;
+        }
+        .mt10 {
+          margin-top: 10px;
+        }
+      `}</style>
+      <style jsx>{`
+        .container {
+          padding: 10px;
+        }
+      `}</style>
+
+      <Menu onClick={handleToggle} defaultCurrent={view} />
+      <div className="container">
+        <Container />
       </div>
-      <div>{m.message}</div>
     </>
   );
 };
