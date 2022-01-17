1048a33e88d94a9bd5c9de5fa718312daf97666b
siykt <cnsiykt@163.com>
2022-01-17 14:41:07 +0800

    feat: 优化chunk的hash计算方式

--- apps/breakpoint-upload/src/app/components/computeHash.worker.ts
+++ apps/breakpoint-upload/src/app/components/computeHash.worker.ts
@@ -1,15 +1,48 @@
-/* eslint-disable no-restricted-globals */
 import SparkMD5 from 'spark-md5';
-import { ChunkInfo } from './BreakpointFileUpload';
+import { DEFAULT_BREAKPOINT_CHUNK_SIZE } from '../constants';
 
-self.onmessage = (e: MessageEvent<ChunkInfo[]>) => {
-  const chunks = e.data;
+function calculateHashSample(file: File) {
+  const OFFSET = DEFAULT_BREAKPOINT_CHUNK_SIZE;
+  const MID_OFFSET = 2;
+  const { size } = file;
+  let cur = OFFSET;
+
+  const chunks = [file.slice(0, OFFSET)];
+  while (cur < size) {
+    if (cur + OFFSET > size) {
+      chunks.push(file.slice(cur));
+    } else {
+      chunks.push(file.slice(cur, cur + MID_OFFSET), file.slice(cur + OFFSET - MID_OFFSET, cur + OFFSET));
+    }
+    cur += OFFSET;
+  }
+
+  return chunks;
+}
+export interface ComputedInfoMessage {
+  percentage: number;
+  hash?: string;
+}
+
+// eslint-disable-next-line
+const ctx: Worker = self as any;
+
+ctx.onmessage = (e: MessageEvent<Blob[] | File>) => {
+  const { data } = e;
+  let chunks: Blob[];
+
+  if (Array.isArray(data)) {
+    chunks = data;
+  } else {
+    chunks = calculateHashSample(data);
+  }
+
   const spark = new SparkMD5.ArrayBuffer();
   let percentage = 0;
   let computedCount = 0;
+
   // 读取内容计算 hash 值
   const load = (event: ProgressEvent<FileReader>) => {
-
     if (typeof event.target.result !== 'string') {
       spark.append(event.target.result);
     } else {
@@ -18,14 +51,15 @@ self.onmessage = (e: MessageEvent<ChunkInfo[]>) => {
     percentage += 100 / chunks.length;
     if (++computedCount === chunks.length) {
       const hash = spark.end();
-      self.postMessage({ percentage: 100, hash });
+      ctx.postMessage({ percentage: 100, hash });
+
       spark.reset();
     } else {
-      self.postMessage({ percentage });
+      ctx.postMessage({ percentage });
     }
   };
 
-  for (const { blob } of chunks) {
+  for (const blob of chunks) {
     const reader = new FileReader();
     reader.readAsArrayBuffer(blob);
     reader.addEventListener('load', load);
